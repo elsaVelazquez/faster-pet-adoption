4419891039b7144d6eb93ed67b79fa7934d9daf7
elsaV <elsa.velazquez@colorado.edu>
2020-07-24 09:10:23 -0600

    clean up sanity check code

--- src/stats/sanity_check_normal_two_tail.py
+++ src/stats/sanity_check_normal_two_tail.py
@@ -19,9 +19,8 @@ sum(females_arr) #sanity check
 males_arr.extend(females_arr)
 sum(males_arr)
 
-import numpy as np
 x = males_arr.extend(females_arr)
-# x
+
 print("\nOriginal male array:")
 print(x)
 r1 = np.mean(x)
@@ -36,11 +35,3 @@ r1= np.var(x)
 r2 = np.mean((x - np.mean(x)) ** 2 )
 assert np.allclose(r1, r2)
 print("\nvariance: ", r1)
-
-
-scipy.stats.ttest_ind()
-# t, p = ttest_ind(a, b, equal_var=False)
-
-
-
-# scipy.stats.ttest_ind(a, b, axis=0, equal_var=True, nan_policy='propagate')
