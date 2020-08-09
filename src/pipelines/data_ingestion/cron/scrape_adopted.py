60cc60c2f09d903faeaf3ef9173d7a33d3770885
elsaV <elsa.velazquez@colorado.edu>
2020-08-08 19:32:50 -0600

    fixed api call

--- src/pipelines/data_ingestion/cron/scrape_adopted.py
+++ src/pipelines/data_ingestion/cron/scrape_adopted.py
@@ -1,24 +1,29 @@
-#for the following cron job
-#10, 20, 30, 40, 50 * * * * scrape_adopted.py > "../data/json/json_dump/$(date +\%d\%m\%y_\%H\%M\%S).json" 2>&1
-
-# curl_beginning = 'curl -o  adopted_page3.csv -H \"Authorization: Bearer '
-
+import os
 from datetime import datetime
-dateTimeObj = datetime.now()
-timestampStr = dateTimeObj.strftime("%d_%b_%Y_%H_%M_%f")
-timestampStr
 
 
-curl_beginning = "curl -o  '../data/json/json_dump/"
 
-out_txt = timestampStr
+def send_curl_cmd(curl_full):
+    return(os.system(curl_full))
 
-curl_middle = ".json -H \"Authorization: Bearer '"
 
-three_hour_token =  open('token.txt', 'r').read()
+if __name__ == "__main__":
 
-curl_end = '\"  https://api.petfinder.com/v2/animals?status=adopted&type=Dog&limit=100'
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d_%b_%Y_%H_%M_%f")
 
-curl_full = curl_beginning + out_txt + curl_middle + three_hour_token + curl_end
-print(curl_full)
-os.system(curl_full)
+
+    curl_beginning = "curl -o  ../../../../data/json/json_dump/"
+
+    out_txt = timestampStr
+
+    curl_middle = ".json -H \"Authorization: Bearer "
+
+    three_hour_token =  open('token.txt', 'r').read()
+
+    curl_end = '\"  https://api.petfinder.com/v2/animals?status=adopted&type=Dog&limit=100'
+
+    curl_full = curl_beginning + out_txt + curl_middle + three_hour_token + curl_end
+    print(curl_full)
+        
+    send_curl_cmd(curl_full)
