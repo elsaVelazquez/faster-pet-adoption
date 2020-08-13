a5615dddb76a58816c5febbe53ce5d51640f65bc
elsaV <elsa.velazquez@colorado.edu>
2020-08-12 19:30:42 -0600

    clean imgs_to_cloud.py

--- src/pipelines/wrangling/AWS/imgs_to_cloud.py
+++ src/pipelines/wrangling/AWS/imgs_to_cloud.py
@@ -1,15 +1,5 @@
 import boto3
-## TODO elsa why doesn't get_file_names work?
-# from pipelines.data_structures import get_file_names
 import os
-
-
-def get_file_names(dir_name):
-    '''
-    For the given path, get the List of all file names in the directory tree 
-    '''
-    return os.listdir(dir_name)
-
 
 def print_s3_contents_boto3(connection):
     '''borrowed from DSI AWS lecture '''
@@ -22,11 +12,11 @@ def print_s3_contents_boto3(connection):
 if __name__ == '__main__':
     
     #path to where all imgs are stored localy
-    dir_str = '../../../../data/img/img_dumps/'
-    print(dir_str)
+    directory_str = '../../../../data/img/img_dumps/'
+    print(directory_str)
     bucket_name = 'faster-pet-adoption.bucket'
     
-    file_names = get_file_names(dir_str)
+    file_names = get_file_names(directory_str)
     print(file_names)
         
     
@@ -37,7 +27,7 @@ if __name__ == '__main__':
 
 
     for file_name in file_names:
-        local_file = dir_str + file_name
+        local_file = directory_str + file_name
 
         s3_client.upload_file(local_file, bucket_name, file_name)
 
