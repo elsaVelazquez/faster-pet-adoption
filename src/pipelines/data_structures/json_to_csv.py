6c189810bdfe05d2081de0538735d8e7cca4760d
elsaV <elsa.velazquez@colorado.edu>
2020-08-16 11:45:00 -0600

    correctly puts edited jsons together into 1 csv

--- src/pipelines/data_structures/json_to_csv.py
+++ src/pipelines/data_structures/json_to_csv.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-        
-    df = pd.read_json (r'../../../data/json/big_json_v4.json')
-    df.to_csv (r'../../../data/csv/aggregated_csv_2.csv', index = None)
+    # file_name = '../../../data/json/clean_giant_json.json'
+    df = pd.read_json (r'../../../data/json/prepped_for_csv/all_dogs_copied_off_console.json')
+    # dirName = ../../../data/json/prepped_for_csv/all_dogs_copied_off_console.json
+  
+    
+    # df = pd.read_json (r'../../../data/json/json_dump_by_animal/animal_id_48549546.JSON')
+    # 
+    # df = pd.read_json (r'../../../data/json/json_dump/animal_id_48549546.JSON')
+    # df = pd.read_json (r'../../../data/json/big_json_v4.json')
+    # df = /Users/elsa/Documents/faster-pet-adoption/src/pipelines/EDA/clean_giant_json.json
+    df.to_csv (r'../../../data/csv/all_dogs_copied_off_console_json_to_csv.csv', index = None)
