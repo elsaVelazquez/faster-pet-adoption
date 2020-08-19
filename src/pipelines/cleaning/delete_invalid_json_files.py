215bacbf3faab2ef8b8d36f638f27f27c8961813
elsaV <elsa.velazquez@colorado.edu>
2020-08-18 20:25:00 -0600

    works perfectly with latest API JSON format

--- src/pipelines/cleaning/delete_invalid_json_files.py
+++ src/pipelines/cleaning/delete_invalid_json_files.py
@@ -31,37 +31,53 @@ def delete_404_error_jsons(list_of_json_files):
 
 
 if __name__ == "__main__":
-
+    #remove cached file from the API return
+    remove_hidden_file = 'rm -f .DS_S'
+    os.system(remove_hidden_file)
     
-    dir_name = '../../../data/json/json_dump_by_animal'
+    
+    dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
+    # dir_name = '../../data/json/json_dump_by_animal'
 
     list_of_files = get_file_names(dir_name)
+    # print(list_of_files)
             
     for file in range(len(list_of_files)):     
         file_name = list_of_files[file] 
-        # print(file_name)
-        with open(file_name, 'r') as file:
+        print(file_name)
+        # print("\n")
+        with open(file_name, 'rb') as file:
+            print(f"opened file {file_name}")
+            print("\n")
+
+        
             zinput1 = '"status":404'
             zinput2 = '"status":429'
             zinput3 = '"type":"Cat","species":"Cat"'
-            zinput4 = "type":"Dog","species":"Dog"'
+            zinput4 = '"type":"Dog","species":"Dog"'
             for line in file:    
-                if (zinput1 in line): # or (zinput2 in line): 
+                if (zinput1.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR 404 file not found, deleted {file_name}") # {file_name}")
-                if (zinput2 in line): # or (zinput2 in line): 
+                    # file.close()
+                if (zinput2.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR 429 exceeded download rate limit, deleted {file_name}")
-                if (zinput3 in line): # or (zinput2 in line): 
+                    # file.close()
+                if (zinput3.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR cat record, deleted {file_name}")
-                # TODO elsa check if this works on mares or whatnot
-                if (zinput4 not in line):
+                #     file.close()
+                # # TODO elsa check if this works on mares or whatnot
+                if (zinput4.encode(encoding='UTF-8') not in line):
                     os.remove(file_name)
                     print(f"ERROR was not a dog species, deleted {file_name}")
+                #     file.close()
+                else:
+                    print(f"kept file {file_name}")
 
             file.close()
                                     
