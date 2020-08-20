d01863371aa3da4d361054a89e5ea9bf28ee5f81
elsaV <elsa.velazquez@colorado.edu>
2020-08-19 18:27:22 -0600

    correctly cycles through each error type

--- src/pipelines/cleaning/delete_invalid_json_files.py
+++ src/pipelines/cleaning/delete_invalid_json_files.py
@@ -36,16 +36,16 @@ if __name__ == "__main__":
     os.system(remove_hidden_file)
     
     
-    dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
-    # dir_name = '../../data/json/json_dump_by_animal'
+    # dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
+    dir_name = '../../../data/json/json_dump_by_animal'
 
     list_of_files = get_file_names(dir_name)
-    # print(list_of_files)
+    print(list_of_files)
             
     for file in range(len(list_of_files)):     
         file_name = list_of_files[file] 
         print(file_name)
-        # print("\n")
+        print("\n")
         with open(file_name, 'rb') as file:
             print(f"opened file {file_name}")
             print("\n")
@@ -60,18 +60,172 @@ if __name__ == "__main__":
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR 404 file not found, deleted {file_name}") # {file_name}")
-                    # file.close()
+                    file.close()
+    #             if (zinput2.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+    #                 # print(line)
+    #                 os.remove(file_name)
+    #                 print(f"ERROR 429 exceeded download rate limit, deleted {file_name}")
+    #                 # file.close()
+    #             if (zinput3.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+    #                 # print(line)
+    #                 os.remove(file_name)
+    #                 print(f"ERROR cat record, deleted {file_name}")
+    #             #     file.close()
+    #             # # TODO elsa check if this works on mares or whatnot
+    #             if (zinput4.encode(encoding='UTF-8') not in line):
+    #                 os.remove(file_name)
+    #                 print(f"ERROR was not a dog species, deleted {file_name}")
+    #             #     file.close()
+    #             else:
+    #                 print(f"kept file {file_name}")
+
+            file.close()
+                                    
+    remove_hidden_file = 'rm -f .DS_S'
+    os.system(remove_hidden_file)
+    
+    
+    
+#######################429 ERROR
+    # dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
+    dir_name = '../../../data/json/json_dump_by_animal'
+
+    list_of_files = get_file_names(dir_name)
+    print(list_of_files)
+            
+    for file in range(len(list_of_files)):     
+        file_name = list_of_files[file] 
+        print(file_name)
+        print("\n")
+        with open(file_name, 'rb') as file:
+            print(f"opened file {file_name}")
+            print("\n")
+
+        
+            # zinput1 = '"status":404'
+            zinput2 = '"status":429'
+            # zinput3 = '"type":"Cat","species":"Cat"'
+            # zinput4 = '"type":"Dog","species":"Dog"'
+            for line in file:    
+                # if (zinput1.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+                #     # print(line)
+                #     os.remove(file_name)
+                #     print(f"ERROR 404 file not found, deleted {file_name}") # {file_name}")
+                #     file.close()
                 if (zinput2.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR 429 exceeded download rate limit, deleted {file_name}")
-                    # file.close()
+                    file.close()
+    #             if (zinput3.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+    #                 # print(line)
+    #                 os.remove(file_name)
+    #                 print(f"ERROR cat record, deleted {file_name}")
+    #             #     file.close()
+    #             # # TODO elsa check if this works on mares or whatnot
+    #             if (zinput4.encode(encoding='UTF-8') not in line):
+    #                 os.remove(file_name)
+    #                 print(f"ERROR was not a dog species, deleted {file_name}")
+    #             #     file.close()
+                else:
+                    print(f"kept file {file_name}")
+
+            file.close()                              
+                                    
+        remove_hidden_file = 'rm -f .DS_S'
+    os.system(remove_hidden_file)
+    
+    
+    
+ ############################################## .  CAT ERROR . #######################
+     # dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
+    dir_name = '../../../data/json/json_dump_by_animal'
+
+    list_of_files = get_file_names(dir_name)
+    print(list_of_files)
+            
+    for file in range(len(list_of_files)):     
+        file_name = list_of_files[file] 
+        print(file_name)
+        print("\n")
+        with open(file_name, 'rb') as file:
+            print(f"opened file {file_name}")
+            print("\n")
+
+        
+            # zinput1 = '"status":404'
+            # zinput2 = '"status":429'
+            zinput3 = '"type":"Cat","species":"Cat"'
+            # zinput4 = '"type":"Dog","species":"Dog"'
+            for line in file:    
+                # if (zinput1.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+                #     # print(line)
+                #     os.remove(file_name)
+                #     print(f"ERROR 404 file not found, deleted {file_name}") # {file_name}")
+                #     file.close()
+                # if (zinput2.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+                #     # print(line)
+                #     os.remove(file_name)
+                #     print(f"ERROR 429 exceeded download rate limit, deleted {file_name}")
+                #     file.close()
                 if (zinput3.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
                     # print(line)
                     os.remove(file_name)
                     print(f"ERROR cat record, deleted {file_name}")
                 #     file.close()
-                # # TODO elsa check if this works on mares or whatnot
+    #             # # TODO elsa check if this works on mares or whatnot
+    #             if (zinput4.encode(encoding='UTF-8') not in line):
+    #                 os.remove(file_name)
+    #                 print(f"ERROR was not a dog species, deleted {file_name}")
+    #             #     file.close()
+                else:
+                    print(f"kept file {file_name}")
+
+            file.close()                              
+                                    
+        remove_hidden_file = 'rm -f .DS_S'
+    os.system(remove_hidden_file)
+    
+    
+    
+    
+###############    NOT DOG  SPECIES ERROR .  #################
+    # dir_name = '/Users/elsa/Documents/faster-pet-adoption/data/json/json_dump_by_animal'   
+    dir_name = '../../../data/json/json_dump_by_animal'
+
+    list_of_files = get_file_names(dir_name)
+    print(list_of_files)
+            
+    for file in range(len(list_of_files)):     
+        file_name = list_of_files[file] 
+        print(file_name)
+        print("\n")
+        with open(file_name, 'rb') as file:
+            print(f"opened file {file_name}")
+            print("\n")
+
+        
+            # zinput1 = '"status":404'
+            # zinput2 = '"status":429'
+            # zinput3 = '"type":"Cat","species":"Cat"'
+            zinput4 = '"type":"Dog","species":"Dog"'
+            for line in file:    
+                # if (zinput1.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+                #     # print(line)
+                #     os.remove(file_name)
+                #     print(f"ERROR 404 file not found, deleted {file_name}") # {file_name}")
+                #     file.close()
+                # if (zinput2.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+                #     # print(line)
+                #     os.remove(file_name)
+                #     print(f"ERROR 429 exceeded download rate limit, deleted {file_name}")
+                #     file.close()
+    #             if (zinput3.encode(encoding='UTF-8') in line): # or (zinput2 in line): 
+    #                 # print(line)
+    #                 os.remove(file_name)
+    #                 print(f"ERROR cat record, deleted {file_name}")
+    #             #     file.close()
+    #             # # TODO elsa check if this works on mares or whatnot
                 if (zinput4.encode(encoding='UTF-8') not in line):
                     os.remove(file_name)
                     print(f"ERROR was not a dog species, deleted {file_name}")
@@ -79,5 +233,7 @@ if __name__ == "__main__":
                 else:
                     print(f"kept file {file_name}")
 
-            file.close()
+            file.close()                              
                                     
+        remove_hidden_file = 'rm -f .DS_S'
+    os.system(remove_hidden_file)
