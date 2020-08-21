abc0b0289422f4b6cf26d5642c2f1aae9c9b9140
elsaV <elsa.velazquez@colorado.edu>
2020-08-21 04:27:38 -0600

    correct paths for json to csv pipeline

--- src/pipelines/data_structures/json_to_csv.py
+++ src/pipelines/data_structures/json_to_csv.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    # file_name = '../../../data/json/clean_giant_json.json'
-    df = pd.read_json (r'../../../data/json/prepped_for_csv/all_dogs_copied_off_console.json')
+    df = pd.read_json (r'../../../data/json/prepped_for_csv/json_prepped_for_csv.json')
+
+    # df = pd.read_json (r'../../../data/json/prepped_for_csv/valid_giant_json_for_csv.json')
+    #latest working one:
+    # df = pd.read_json (r'../../../data/json/prepped_for_csv/all_dogs_copied_off_console.json')
     # dirName = ../../../data/json/prepped_for_csv/all_dogs_copied_off_console.json
   
     
     # df = pd.read_json (r'../../../data/json/json_dump_by_animal/animal_id_48549546.JSON')
     # 
     # df = pd.read_json (r'../../../data/json/json_dump/animal_id_48549546.JSON')
-    # df = pd.read_json (r'../../../data/json/big_json_v4.json')
+    # df = pd.read_json (r'../../../data/json/prepped_for_csv/big_json_v4.json')
     # df = /Users/elsa/Documents/faster-pet-adoption/src/pipelines/EDA/clean_giant_json.json
-    df.to_csv (r'../../../data/csv/all_dogs_copied_off_console_json_to_csv.csv', index = None)
+    df.to_csv (r'../../../data/csv/giant_valid_csv.csv', index = None)
