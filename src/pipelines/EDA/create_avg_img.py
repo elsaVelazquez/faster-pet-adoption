dae511ed9690aaab9c863f73cddca061a3190395
elsaV <elsa.velazquez@colorado.edu>
2020-08-24 15:12:50 -0600

    remove commented out code

---
+++ src/pipelines/EDA/create_avg_img.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import os, sys
+
+path = '../../../data/img/img_dumps/'
+save_adopted_path = '../../../data/img/adopted/'
+save_adoptable_path = '../../../data/img/adoptable/'
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if 'DS_Store' not in item:
+            if os.path.isfile(path+item):
+                im = Image.open(path+item)
+                file, e = os.path.splitext(path+item)
+                imResize = im.resize((450, 450), Image.ANTIALIAS)
+                # imResize.save(file + '_resized.jpg', 'JPEG', quality=90)
+                imResize.save(save_adopted_path + item + '_resized.jpg', 'JPEG', quality=90)
+
+
+
+if __name__ == '__main__':
+    
+    
+    resize()
+
+   
