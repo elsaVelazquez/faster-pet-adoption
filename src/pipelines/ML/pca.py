0317dcb443fa6c0faed1a7455c8784807d042edb
elsaV <elsa.velazquez@colorado.edu>
2020-08-28 02:28:05 -0600

    printed pretty plots

--- src/pipelines/ML/pca.py
+++ src/pipelines/ML/pca.py
@@ -36,19 +36,24 @@ if __name__ == '__main__':
     print("original shape:   ", X.shape)
     print("transformed shape:", X_pca.shape)
     
+    print("X[:, 0]: ", X[:, 0])
+    print("X[:, 1]: ", X[:, 1])
+    
     # TODO ELSA PRINT AND SAVE THIS PLOT
     #Light is original data points, dark is projected data points
     #shows how much "information" is discarded in this reduction of dimensionality.
     #TODO elsa fix this plot
     X_new = pca.inverse_transform(X_pca)
-    plt.scatter(X[:, 0], X[:, 1], alpha=0.2)
-    plt.scatter(X_new[:, 0], X_new[:, 1], alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], alpha=0.2, c='red', label="Discarded Information")
+    plt.scatter(X_new[:, 0], X_new[:, 1], alpha=0.2, c='green', label="Information Kept")
     plt.axis('equal');
+    plt.title('Information Discarded Via Dimensionality Reduction')
+    plt.legend(loc="upper left")
     plt.show()
     plt.close()
     
     
-    
+    # TODO ELSA PRINT AND SAVE THIS PLOT
     pca = PCA(2)  # project from 1024 to 2 dimensions
     projected = pca.fit_transform(X)
     print(X.data.shape)
@@ -56,9 +61,10 @@ if __name__ == '__main__':
     plt.scatter(projected[:, 0], projected[:, 1],
             c=y, edgecolor='none', alpha=0.5,
             cmap=plt.cm.get_cmap('Accent', 10))
-    plt.xlabel('component 1')
-    plt.ylabel('component 2')
-    plt.colorbar();
+    plt.xlabel('component 1, flat img vector from a (1*704) matrix')
+    plt.ylabel('component 2, flat img vector from a (1*704) matrix')
+#     plt.colorbar();
+    plt.title('Dimension Reduction from 704*1024 to 704*1 Matrix')
     plt.show()
     plt.close()
     
