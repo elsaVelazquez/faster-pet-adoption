c0f43a34fb12b37b979a0906ff221d4e5ecd26c0
elsaV <elsa.velazquez@colorado.edu>
2020-09-15 15:36:42 -0600

    rolled back working css

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 
 from flask import Flask, render_template, request, jsonify
 import pickle
-from build_model_pickled import * 
+from predict_one import TextClassifier
+
+#instantiate the class
+clf = TextClassifier()
+
 
 app = Flask(__name__)
 
-
-with open('static/model.pkl', 'rb') as f:
-    model = pickle.load(f)
 
 
 @app.route('/', methods=['GET'])
@@ -23,27 +24,32 @@ def submit():
     description to be classified.  """
     return render_template('submit.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     """Recieve the description to be classified from an input form and use the
     model to classify.
     """
+    
     # breakpoint()
     data = str(request.form['article_body'])
-    pred = str(model.sentim([data])[0])
-    sent = str(model.sentim([data])[0])
-    twit = str(model.sentim_twitter([data]))#[0])
-    return render_template('predict.html', description=data, predicted=pred, sentiment=sent, twitter=twit)
+
+    pred = str(clf.predict_one([data])) #[0])
+    # twit = str(model.predict_one_twitter([data]))#[0])
+    return render_template('predict.html', description=data, predicted=pred) 
+#, twitter=twit)
+
 
 @app.route('/about', methods=['GET'])
 def about():
-    """Render a page containing a textarea input where the user can paste an
-    description to be classified.  """
+    """More about this project """
     return render_template('about.html')
 
+@app.route('/contact', methods=['GET'])
+def contact():
+    """contact Elsa"""
+    return render_template('contact.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5013, debug=True)
+    app.run(host='0.0.0.0', port=5028, debug=True)
     
-    
-#
 
