0d60828e0c58607e2fd2f232b00d34ca9524b6c9
elsaV <elsa.velazquez@colorado.edu>
2020-09-15 16:12:57 -0600

    add twitter dataset prediction

--- app/predict_one.py
+++ app/predict_one.py
@@ -21,11 +21,8 @@ from sklearn.metrics import f1_score
 class TextClassifier(object):
 
     def __init__(self):
-        # self._vectorizer = TfidfVectorizer()
-        # self._classifier = MultinomialNB()
-        # self.data = data
-        # self.model = MultinomialNB()
         
+        #import dog database data
         with open('pickled_algos/pickled_nb.pickle', 'rb') as f:
             self.model = pickle.load(f)
 
@@ -33,6 +30,16 @@ class TextClassifier(object):
             self.tfidf = pickle.load(f)
 
         with open('pickled_algos/count_vect.pickle', 'rb') as f:
+            self.cv = pickle.load(f)
+            
+        #import twitter data
+        with open('pickled_algos/pickled_nb_tweets.pickle', 'rb') as f:
+            self.model = pickle.load(f)
+
+        with open('pickled_algos/tfidf_transformer_tweets.pickle', 'rb') as f:
+            self.tfidf = pickle.load(f)
+
+        with open('pickled_algos/count_vect.pickle_tweets', 'rb') as f:
             self.cv = pickle.load(f)
    
     def predict_one(self, data):
@@ -45,13 +52,30 @@ class TextClassifier(object):
         else:
             res = ("Positive Sentiment")
         return res 
+    
+    def predict_one_twitter(self, data):
+        cv_transformed = self.cv.transform(data) #counts how many words
+        tfidf_transformed = self.tfidf.transform(cv_transformed)  #tf == cv . 
+        string_predicted = self.model.predict(tfidf_transformed) 
+        res = str(string_predicted[0])
+        if res == '0':
+            res = ('Negative Sentiment')
+        else:
+            res = ("Positive Sentiment")
+        return res 
 
 if __name__ == '__main__':
-
-    my_classifier = TextClassifier()
-    test_string_pred = ['this girl is a foster pit and has none of her teeth']
-    res = my_classifier.predict_one(test_string_pred)
-    print(res)
+    #instantiate object
+    # my_classifier = TextClassifier()
+    
+    # #test negative sentiment
+    # test_string_pred = ['this girl is a foster pit and has none of her teeth']
+    
+    # #test negative sentimnet
+    # test_string_pred = ['fill out an online form to find this male puppy a forever home']
+    
+    # res = my_classifier.predict_one(test_string_pred)
+    # print(res)
 
 
     
